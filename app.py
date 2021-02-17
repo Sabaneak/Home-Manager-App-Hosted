4a4feaba135f4145c499c6df7f0b46bf28946700
Sabaneak <akhilmacherla8@gmail.com>
2021-02-17 12:27:23 +0530

    Heroku Try 4

--- app.py
+++ app.py
@@ -30,15 +30,10 @@ CORS(app)
 api = Api(app)
 jwt = JWTManager(app)
 migrate = Migrate(app, db)
-db.init_app(app)
-ma.init_app(app)
 initialize_routes(api)
 initialize_oauth(app)
 
-@app.before_first_request
-def table():
-    db.create_all()
-
 if __name__ == '__main__':
-    
+    db.init_app(app)
+    ma.init_app(app)
     app.run(debug=True, use_reloader=False)  
